a436adfffdee40e4068df0b9a2e3df03653ff90b
Adrian Pascu <adrian@pascu.be>
2025-04-22 02:01:43 +0200

    Make timer flash when expired

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { app, Tray, Menu, nativeImage } from 'electron'
+import ansiStyles from 'ansi-styles';
 
 const formatTime = (seconds) => {
   const minutes = Math.floor(seconds / 60)
@@ -11,24 +12,40 @@ const DURATION = 5;
 (async () => {
   let secondsLeft = null
   let timer = null
+  let flashInterval = null
+
+  const flashMenuBar = () => {
+    let isGreen = true;
+    return setInterval(() => {
+      if (isGreen) {
+        tray.setTitle(`${ansiStyles.bgGreen.open}Time's up!${ansiStyles.bgGreen.close}`);
+      } else {
+        tray.setTitle("Time's up!");
+      }
+      isGreen = !isGreen;
+    }, 500);
+  };
 
   const startTimer = () => {
-    secondsLeft = DURATION
-    tray.setTitle(formatTime(secondsLeft))
+    secondsLeft = DURATION;
+    tray.setTitle(formatTime(secondsLeft));
     timer = setInterval(() => {
-      secondsLeft -= 1
+      secondsLeft -= 1;
       if (secondsLeft <= 0) {
-        clearInterval(timer)
-        tray.setTitle('Time\'s up!')
+        clearInterval(timer);
+        tray.setTitle("Time's up!");
+        flashInterval = flashMenuBar();
       } else {
-        tray.setTitle(formatTime(secondsLeft))
+        tray.setTitle(formatTime(secondsLeft));
       }
-    }, 1000)
-  }
+    }, 1000);
+  };
 
   const resetTimer = () => {
-    clearInterval(timer)
-    startTimer()
+    clearInterval(timer);
+    clearInterval(flashInterval);
+    tray.setTitle("Timer Reset");
+    startTimer();
   }
 
   await app.whenReady()
