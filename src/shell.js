0dab73f7da9fcce7599f5e4566af302aca7fe320
Adrian Pascu <adrian@pascu.be>
2026-09-02 10:00:45 +0200

    Show the Wi-Fi name on the charger menu item

---
+++ src/shell.js
@@ -0,0 +1,10 @@
+import { execFile } from 'node:child_process'
+
+const COMMAND_TIMEOUT_MS = 5000
+
+export const run = (command, args, input = '') =>
+  new Promise((resolve) => {
+    const child = execFile(command, args, { timeout: COMMAND_TIMEOUT_MS }, (error, stdout) => resolve(error ? '' : stdout))
+    child.stdin.on('error', () => {})
+    child.stdin.end(input)
+  })
